232228607cb88354ba4bedd05c3bb02b50d0edf1
osthorp1603-spec <osthorp1603@gmail.com>
2025-11-12 14:31:56 -0500

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,12 +1,9 @@
-import mysql from "mysql2/promise";
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-export const conectar = async () => {
-  const conexion = await mysql.createConnection({
-    host: "localhost",
-    user: "oscar",
-    password: "Bouncing", // ⬅️ tu nueva contraseña aquí
-    database: "examenes",
-  });
-  console.log("✅ Conexión MySQL establecida correctamente");
-  return conexion;
-};
+function conectar() {
+  const rutaBD = path.resolve(__dirname, "db", "resultados.db");
+  return new sqlite3.Database(rutaBD);
+}
+
+module.exports = { conectar };
