c661501bf9fba8080f6c1eb9193099e9f0fd75d5
osthorp1603-spec <osthorp1603@gmail.com>
2025-11-11 22:30:57 -0500

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 function conectar() {
-  const rutaBD = path.resolve(__dirname, "db", "respuestas.db");
+  const rutaBD = path.resolve(__dirname, "db", "resultados.db");
   return new sqlite3.Database(rutaBD);
 }
 
